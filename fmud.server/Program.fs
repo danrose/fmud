b7e9eda66a0b8354e5df2a05a67dd4b9a176202f
Dan Rose <dan.rose.uk@gmail.com>
2015-01-25 17:54:45 +0000

    Simple server->client connection using signalr

---
+++ fmud.server/Program.fs
@@ -0,0 +1,38 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+
+open Owin
+open Microsoft.AspNet.SignalR
+open Microsoft.AspNet.SignalR.Hubs
+open Microsoft.Owin.Hosting
+open Microsoft.Owin.Cors
+open System
+open Microsoft.Owin
+
+type Startup() =
+     member this.Configuration(app:IAppBuilder) =
+        app.UseCors(CorsOptions.AllowAll) |> ignore
+        app.MapSignalR() |> ignore
+
+[<assembly: OwinStartup(typeof<Startup>)>]
+do ()
+
+type GameHub() =
+    inherit Hub()
+    member this.AcceptCommand(what: string) =
+        let all = this.Clients.All :?> ClientProxy
+        printfn "Received '%s' from %A" what this.Context.ConnectionId
+        all.Invoke("Print", "Huh? " + what)
+
+
+[<EntryPoint>]
+let main argv = 
+    let url = "http://127.0.0.1:8088/"
+    use app = WebApp.Start(url)
+    printfn "Started server on %s" url
+    let mutable input = ""
+    while(input <> "quit") do
+        
+        input <- Console.ReadLine()
+        
+    0
---
+++ fmud/Program.fs
@@ -0,0 +1,22 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+
+open Microsoft.AspNet.SignalR
+open Microsoft.AspNet.SignalR.Client
+open System
+
+[<EntryPoint>]
+let main argv = 
+    let url = "http://127.0.0.1:8088/"
+    
+    let connection = new HubConnection(url)
+    let proxy = connection.CreateHubProxy("GameHub")
+    let callback = new Action<string>(fun msg -> printfn "Received %s" msg)
+    proxy.On<string>("Print", callback) |> ignore
+    Async.AwaitIAsyncResult(connection.Start())
+    printfn "Client connected to %s" url
+    let mutable input = ""
+    while true do
+        input <- Console.ReadLine()
+        proxy.Invoke("AcceptCommand", input) |> ignore
+    0
