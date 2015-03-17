59c11cde2a7febf87604241b715527dad28cb6ea
Dan Rose <dan.rose.uk@gmail.com>
2015-03-17 20:38:19 +0000

    Small update to use an overloaded operator for Asserting.

--- fmud.tests/SetOperators.fs
+++ fmud.tests/SetOperators.fs
@@ -5,23 +5,25 @@ open fmud
 
 [<TestFixture>]
 type SetOperators() = 
+    let (===) (x:obj) (y:obj) =
+        Assert.AreEqual(x, y)
 
     [<Test>]
     member this.SimpleAddEntry() =
         let set = Set.empty.Add("abc").Add("def");
-        Assert.AreEqual(set @+ "foo", Set.empty.Add("abc").Add("def").Add("foo"))
+        set @+ "foo" === Set.empty.Add("abc").Add("def").Add("foo")
 
     [<Test>]
     member this.AddMultipleEntries() =
         let set = Set.empty.Add("abc").Add("def");
-        Assert.AreEqual(set @+ "foo bar ping", Set.empty.Add("abc").Add("def").Add("foo").Add("bar").Add("ping"))
+        set @+ "foo bar ping" === Set.empty.Add("abc").Add("def").Add("foo").Add("bar").Add("ping")
 
     [<Test>]
     member this.SimpleRemoveEntry() =
         let set = Set.empty.Add("abc").Add("def");
-        Assert.AreEqual(set @- "abc", Set.empty.Add("def"))
+        set @- "abc" === Set.empty.Add("def")
 
     [<Test>]
     member this.RemoveMultipleEntries() =
         let set = Set.empty.Add("abc").Add("def");
-        Assert.AreEqual(set @- "def abc", Set.empty)
+        set @- "def abc" === Set.empty
